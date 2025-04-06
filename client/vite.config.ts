1193623e1f7131741ecbe3c371d19ba73013b09c
Abdo Magdy <amagdy3@gmail.com>
2025-04-06 07:34:45 +0200

    Fix CSS Issues

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    postcss: './postcss.config.js',
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
